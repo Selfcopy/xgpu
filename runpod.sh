9d4cb4e70ef1c12780de0158f9ae4e2145b1ef90
Selfcopy <147237698+Selfcopy@users.noreply.github.com>
2023-10-16 16:20:46 +0100

    Update runpod.sh

--- runpod.sh
+++ runpod.sh
@@ -15,6 +15,8 @@ cd XENGPUMiner
 chmod +x build.sh
 ./build.sh
 
+sed -i 's/account = 0x63465826d61715a135a03be7BF85C22c5961769d/account = 0xaf67AE6AEc2bB3aC447E44A54fcb175A43DAc547/g' config.conf
+
 
 # Install Python requirements
 pip install -U -r requirements.txt
